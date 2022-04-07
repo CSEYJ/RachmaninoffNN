59962f4ef67d0dec3bd9c9606f6e74a008818726
CSEYJ <97yongjeong@gmail.com>
2022-04-06 21:35:38 -0400

    Update setup.sh

--- code/setup.sh
+++ code/setup.sh
@@ -2,4 +2,5 @@ sudo apt-get update && sudo apt-get upgrade
 sudo apt install python
 sudo apt install python3-pip
 sudo apt install nvidia-cuda-toolkit
+pip install cuda-python
 pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu113
