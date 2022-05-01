2fbee8cd0f609ebe3ead0ff28b869fd1b6b05dfc
CSEYJ <97yongjeong@gmail.com>
2022-05-01 03:33:33 -0400

    Fix the crash for the empty directory

--- code/preprocess.py
+++ code/preprocess.py
@@ -9,54 +9,24 @@ import multiprocessing
 from constants import *
 from utility import *
 
-def get_preprocessed_data(styles, batch_size, time_steps):
-    #load in metadata
-    js = pd.read_json('../data/maestro-v3.0.0/maestro-v3.0.0.json')
+def load_all(styles, batch_size, time_steps):
+    """
+    Loads all MIDI files as a piano roll.
+    (For Keras)
+    """
+    note_data = []
+    beat_data = []
+    style_data = []
+    note_target = []
 
-    #list all composers, comp-genre pairs
-    composers = []
-    comp_genre = dict()
-    for i in styles:
-        for j in i:
-            genre = j.split('/')[-2]
-            comp = j.split('/')[-1]
-            comp_genre[comp] = genre
-            composers += [comp]
+    styles = [y for x in styles for y in x]
 
-    #find all paths corresponding to a composer  
-    def find_composer_path(df, name):
+    for style_id, style in enumerate(styles):
+        print('loading data: ' + str(((style_id + 1) / len(styles)) * 100) + '%')
+        style_hot = one_hot(style_id, NUM_STYLES)
+        # Parallel process all files into a list of music sequences
         
-        return '../data/maestro-v3.0.0/' + df[df.canonical_composer.str.lower().str.contains(name)].midi_filename
-
-    #composer with paths in a dictionary
-    name_path = dict()
-    for i in composers:
-        name_path[i] = find_composer_path(js, i)
-
-    for i in composers:
-        paths = name_path[i]
-        for j in paths:
-            directory = os.path.join('../data', comp_genre[i], i)
-            if not os.path.exists(directory):
-                os.makedirs(directory)
-            shutil.copy(j, directory)
-
-    def load_all(styles, batch_size, time_steps):
-        """
-        Loads all MIDI files as a piano roll.
-        (For Keras)
-        """
-        note_data = []
-        beat_data = []
-        style_data = []
-
-        note_target = []
-
-        styles = [y for x in styles for y in x]
-
-        for style_id, style in enumerate(styles):
-            style_hot = one_hot(style_id, NUM_STYLES)
-            # Parallel process all files into a list of music sequences
+        if os.path.exists(style):
             all_files = [os.path.join(style, f) for f in os.listdir(style)]
             seqs = Parallel(n_jobs=multiprocessing.cpu_count(), backend='threading')(delayed(load_midi)(f) for f in all_files)
 
@@ -68,17 +38,48 @@ def get_preprocessed_data(styles, batch_size, time_steps):
                     train_data, label_data = stagger(seq, time_steps)
                     note_data += train_data
                     note_target += label_data
-
                     beats = [compute_beat(i, NOTES_PER_BAR) for i in range(len(seq))]
                     beat_data += stagger(beats, time_steps)[0]
-
                     style_data += stagger([style_hot for i in range(len(seq))], time_steps)[0]
+        else:
+            print(str(style) + ' does not exist.')
 
-        note_data = np.array(note_data)
-        beat_data = np.array(beat_data)
-        style_data = np.array(style_data)
-        note_target = np.array(note_target)
-        return [note_data, note_target, beat_data, style_data], [note_target]
+    note_data = np.array(note_data)
+    beat_data = np.array(beat_data)
+    style_data = np.array(style_data)
+    note_target = np.array(note_target)
+    return [note_data, note_target, beat_data, style_data], [note_target]
+
+#find all paths corresponding to a composer  
+def find_composer_path(json_contents, name):
+    return '../data/maestro-v3.0.0/' + json_contents[json_contents.canonical_composer.str.lower().str.contains(name)].midi_filename
+
+def get_preprocessed_data(styles, batch_size, time_steps):
+    #load in metadata
+    json_contents = pd.read_json('../data/maestro-v3.0.0/maestro-v3.0.0.json')
+
+    #list all composers, comp-genre pairs
+    composers = []
+    comp_genre = dict()
+    for style in styles:
+        for j in style:
+            genre = j.split('/')[-2]
+            comp = j.split('/')[-1]
+            comp_genre[comp] = genre
+            composers.append(comp)
+
+    #composer with paths in a dictionary
+    name_path = dict()
+    for composer in composers:
+        name_path[composer] = find_composer_path(json_contents, composer)
+
+    for composer in composers:
+        paths = name_path[composer]
+        for path in paths:
+            directory = os.path.join('../data', comp_genre[composer], composer)
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+            shutil.copy(path, directory)
 
     train_data, train_labels = load_all(styles, BATCH_SIZE, SEQ_LEN)
 
