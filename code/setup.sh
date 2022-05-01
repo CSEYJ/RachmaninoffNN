48ff6ebab9fe01b0d6bc225d1c4aef754a271ed2
CSEYJ <97yongjeong@gmail.com>
2022-05-01 05:18:31 -0400

    Fix the compatibility between torch and torchvision

--- code/setup.sh
+++ code/setup.sh
@@ -5,5 +5,5 @@ sudo apt -y install python3-pip
 sudo apt -y install libasound2-dev swig
 sudo apt -y install nvidia-cuda-toolkit
 pip3 install -r requirements.txt
-pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu113
+pip3 install torchaudio --extra-index-url https://download.pytorch.org/whl/cu113
 pip3 install git+https://github.com/vishnubob/python-midi@feature/python3
