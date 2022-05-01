673390ff9c5b26c5a06a97586d57bfc2006af6cd
CSEYJ <97yongjeong@gmail.com>
2022-05-01 02:17:03 -0400

    Fix setup bug (python3)

--- code/setup.sh
+++ code/setup.sh
@@ -4,6 +4,6 @@ sudo apt -y install python
 sudo apt -y install python3-pip
 sudo apt -y install libasound2-dev swig
 sudo apt -y install nvidia-cuda-toolkit
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu113
-pip install git+https://github.com/vishnubob/python-midi@feature/python3
+pip3 install git+https://github.com/vishnubob/python-midi@feature/python3
