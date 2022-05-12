73a9bdf2b15502a04ccdb09a6582d6afb1821b25
Yongjeong Kim <yongjeong_kim@brown.edu>
2022-05-12 03:55:21 -0400

    Delete download_large.sh

--- code/download_large.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-curl https://storage.googleapis.com/magentadata/datasets/maestro/v3.0.0/maestro-v3.0.0.zip --output ../data/maestro-v3.0.0.zip 
-unzip ../data/maestro-v3.0.0.zip -d ../data/
-rm ../data/maestro-v3.0.0.zip
